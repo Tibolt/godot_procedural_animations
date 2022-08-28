d8cbb649e6b2581ea8e1c0e4f194113d4af04fff
Tibolt <tibo.ak015@gmail.com>
2022-08-29 01:22:00 +0200

    early prototype

--- src/player/Player.cs
+++ src/player/Player.cs
@@ -17,7 +17,12 @@ public class Player : KinematicBody
 	private SpringArm springArm {get; set;}
 	private Spatial pivot {get; set;}
 	private Spatial model {get; set;}
-	private Position3D body {get; set;}
+	private Skeleton skeleton {get; set;}
+	private RayCast bodyRay {get; set;}
+	private Position3D leftTarget {get; set;}
+	private Position3D rightTarget {get; set;}
+	private RayCast rayTargetLeft {get; set;}
+	private RayCast rayTargetRight {get; set;}
 	private Position3D moveTarget {get; set;}
 
 	private BoneAttachment leftFoot {get; set;}
@@ -27,15 +32,14 @@ public class Player : KinematicBody
 	private Position3D targetContL {get; set;}
 	private Position3D targetContR {get; set;}
 
-    private CollisionShape targetSphereL {get;set;}
-
 	private RayCast raycastL {get; set;}
 	private RayCast raycastR {get; set;}
 
 	private RayCast raycastTargetL {get; set;}
 	private RayCast raycastTargetR {get; set;}
 
-	public RayCast ray = new RayCast();
+	private CollisionShape collsion {get;set;}
+
 	
 	/* private float Length = 5.0f; */
 
@@ -44,8 +48,13 @@ public class Player : KinematicBody
 	{
 		springArm = GetNode<SpringArm>("SpringArm");
 		pivot = GetNode<Spatial>("Pivot");
-		model = GetNode<Spatial>("Pivot/Robot/Body");
-		body = GetNode<Position3D>("Pivot/Robot/Body");
+		model = GetNode<Spatial>("Pivot/Robot");
+		skeleton = GetNode<Skeleton>("Pivot/Robot/Armature/Skeleton");
+		bodyRay = GetNode<RayCast>("Pivot/Robot/Armature/Skeleton/BodyRay");
+		leftTarget = GetNode<Position3D>("Pivot/Robot/TargetLeft");
+		rightTarget = GetNode<Position3D>("Pivot/Robot/TargetRight");
+		rayTargetLeft = GetNode<RayCast>("Pivot/Robot/TargetLeft/RayTargetLeft");
+		rayTargetRight = GetNode<RayCast>("Pivot/Robot/TargetRight/RayTargetRight");
 		moveTarget = GetNode<Position3D>("moveTarget");
 
 		leftFoot = GetNode<BoneAttachment>("Pivot/Robot/Armature/Skeleton/left_foot");
@@ -55,8 +64,6 @@ public class Player : KinematicBody
 		targetContL = GetNode<Position3D>("Pivot/target_container_left");
 		targetContR = GetNode<Position3D>("Pivot/target_container_right");
 
-        targetSphereL = GetNode<CollisionShape>("TargetCubeL");
-
 		targetContL.SetAsToplevel(true);
 		targetContR.SetAsToplevel(true);
 
@@ -65,24 +72,25 @@ public class Player : KinematicBody
 		raycastTargetL = GetNode<RayCast>("Pivot/raycast_target_left");
 		raycastTargetR = GetNode<RayCast>("Pivot/raycast_target_right");
 
-		ikLeftFoot.Start(false);
-		ikRightFoot.Start(false);
+		collsion = GetNode<CollisionShape>("CollisionShape");
+
+		ikLeftFoot.Start();
+		ikRightFoot.Start();
 		
-		body.AddChild(ray);
-		ray.Enabled = true;
-		ray.CastTo = Vector3.Down * 3;
 	}
 
-	public override void _PhysicsProcess(float delta)
+	public async override void _PhysicsProcess(float delta)
 	{
 		Move(delta);
-		/* Anim(); */
+		/* Step(); */
+		IKStep(targetContL, leftTarget, 4f, raycastTargetL);
+		await ToSignal(GetTree().CreateTimer(0.2f), "timeout");
+		IKStep(targetContR, rightTarget, 4f, raycastTargetR);
 
 	}
-	public void Anim()
+	public void Step()
 	{
-		var length = 4;
-		ray.Translation = body.Translation;
+		var length = 3;
 		//LEFT
 		var targetL = targetContL.Translation;
 
@@ -93,7 +101,8 @@ public class Player : KinematicBody
 			var contactPoint = raycastL.GetCollisionPoint();
 			targetContL.GlobalTranslation = contactPoint;
 			
-			/* if(CheckFootDistance(leftFoot, this.GlobalTranslation.z - length)) */
+			/* if(CheckFootDistance(leftFoot, playerPos, length)) */
+			/* 	UpdateIK(targetContL, targetSphereL, raycastTargetL); */
 			/* 	MoveLegToTarget(targetContL, raycastTargetL); */
 
 		}
@@ -121,7 +130,7 @@ public class Player : KinematicBody
 				MoveLegToTarget(targetContR, raycastTargetR);
 		}
 	}
-	public void UpdateIK(Position3D leg, Position3D target)
+	public void UpdateIK(Position3D leg, CollisionShape target, RayCast rayTarget)
 	{
 		var minDistance = 5f;
 
@@ -132,6 +141,12 @@ public class Player : KinematicBody
 			offset = (offset / distanceToTarg) * minDistance;
 			distanceToTarg = minDistance;
 		}
+
+		var colY = rayTarget.GetCollisionPoint().y;
+		var targ = target.GlobalTranslation;
+
+		leg.GlobalTranslation = leg.GlobalTranslation.LinearInterpolate(targ, 0.1f);
+
 	}
 	/* public Position3D MakeStep(Position3D legPos,Position3D tPos) */
 	/* { */
@@ -159,21 +174,93 @@ public class Player : KinematicBody
 	}
 	public override void _Process(float delta)
 	{
-		/* springArm.Translation = this.Translation; */
-		Anim();
+		var springPos = springArm.Translation;
+		springPos.z = this.Translation.z;
+		springArm.Translation = springPos;
+
+		/* ChangeTargetDir(leftTarget, Direction); */
+
+		RayTarget(rayTargetLeft ,leftTarget);
+		RayTarget(rayTargetRight ,rightTarget);
+
+		LowerBody();
+
 	}
+	public void IKStep(Position3D pos, Position3D target, float distance, RayCast ray)
+	{
+		var last = pos.GlobalTranslation;
+		last.y = raycastTargetL.GlobalTranslation.y;
+		ray.GlobalTranslation =  last;
+
+		if(target.GlobalTranslation.DistanceTo(pos.GlobalTranslation) > distance)
+		{
+
+			pos.GlobalTranslation = pos.GlobalTranslation.LinearInterpolate(target.GlobalTranslation, 0.9f);
+			/* pos.GlobalTranslation = target.GlobalTranslation; */
+
+			GD.Print(target.GlobalTranslation.DistanceTo(pos.GlobalTranslation));
+		}
+	}
+
+	public void RayTarget(RayCast ray, Position3D curr)
+	{
 		
+		if(ray.IsColliding())
+		{
+			var pos = curr.GlobalTranslation;
+			pos.y = rayTargetLeft.GetCollisionPoint().y;
+			curr.GlobalTranslation = pos;
+			
+			//test
+			/*             var c = collsion.GlobalTranslation; */
+			/* c.y = rayTargetLeft.GetCollisionPoint().y + 1f; */
+			/*             collsion.GlobalTranslation = c; */
+		}
+	}
+	public void LowerBody()
+	{
+		var height = 0f;
+
+		var pos = model.GlobalTranslation;
+		if(bodyRay.IsColliding())
+		{
+			pos.y = bodyRay.GetCollisionPoint().y + height;
+		}
+		model.GlobalTranslation = pos;
+	}
+
+	public void ChangeTargetDir(Position3D pos, Vector3 direction)
+	{
+		var currPos = pos.GlobalTranslation;
+		if(direction == Vector3.Back)
+			currPos.z = -currPos.z;
+		pos.GlobalTranslation = currPos;
+	}
 	public override void _Input(InputEvent @event)
 	{
 
-		if(@event.IsActionPressed("forward"))
-			Direction += new Vector3(0,0,-1);
-		else if(@event.IsActionPressed("back"))
-			Direction += new Vector3(0,0,1);
-		if(@event.IsActionPressed("right"))
-			Direction += new Vector3(1,0,0);
-		else if(@event.IsActionPressed("left"))
-			Direction += new Vector3(-1,0,0);
+		/* if(@event.IsActionPressed("forward")) */
+		/* 	Direction += new Vector3(0,0,-1); */
+		/* else if(@event.IsActionPressed("back")) */
+		/* 	Direction += new Vector3(0,0,1); */
+		/* if(@event.IsActionPressed("right")) */
+		/* 	Direction += new Vector3(1,0,0); */
+		/* else if(@event.IsActionPressed("left")) */
+		/* 	Direction += new Vector3(-1,0,0); */
+		
+		var curr = model.Translation;
+		if(@event.IsActionPressed("ctrl"))
+		{
+			/* var curr = model.Translation; */
+			curr.y = -0.5f;
+			model.Translation = curr;
+		}
+		else
+		{
+			curr.y = 0f;
+			model.Translation = curr;
+		}
+
 		
 		if(@event.IsActionPressed("jump"))
 			IsJumping = true;
